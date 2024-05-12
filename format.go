d86fb60f1690160e09874b2d10835a48fbada005
nametake <nametake.kyarabuki@gmail.com>
2024-05-13 00:54:37 +0900

    Replaced to items

--- format.go
+++ format.go
@@ -7,12 +7,21 @@ import (
 	"cloud.google.com/go/spanner/spansql"
 )
 
+type Item interface {
+	Pos() spansql.Position
+	SQL() string
+}
+
 type DDLItem struct {
 	ddl spansql.DDLStmt
 }
 
 func (d *DDLItem) Pos() spansql.Position {
 	return d.ddl.Pos()
+}
+
+func (d *DDLItem) SQL() string {
+	return d.ddl.SQL()
 }
 
 type CommentItem struct {
@@ -23,15 +32,26 @@ func (c *CommentItem) Pos() spansql.Position {
 	return c.comment.Pos()
 }
 
+func (c *CommentItem) SQL() string {
+	// return c.comment.SQL()
+	return ""
+}
+
 func FormatDDL(ddlStr string) (string, error) {
 	parsedDDL, err := spansql.ParseDDL("f", ddlStr)
 	if err != nil {
 		return "", fmt.Errorf("parse DDL: %v", err)
 	}
 
-	var sqls []string
+	items := make([]Item, 0)
+
 	for _, ddl := range parsedDDL.List {
-		sql := fmt.Sprintf("%s;", ddl.SQL())
+		items = append(items, &DDLItem{ddl})
+	}
+
+	var sqls []string
+	for _, item := range items {
+		sql := fmt.Sprintf("%s;", item.SQL())
 		sqls = append(sqls, sql)
 	}
 
--- format_test.go
+++ format_test.go
@@ -56,8 +56,8 @@ CREATE CHANGE STREAM SingerAlbumStream FOR Singers, Albums;`,
 		{
 			name: "Comment block",
 			ddl: `CREATE TABLE Album (ID STRING(MAX) NOT NULL, Title STRING(MAX) NOT NULL, Artist STRING(MAX) NOT NULL, CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true)) PRIMARY KEY(ID);
--- COMMENT Block 1
--- COMMENT Block 2
+-- COMMENT 1
+-- COMMENT 2
 CREATE CHANGE STREAM SingerAlbumStream FOR Singers, Albums;
 			`,
 			want: `CREATE TABLE Album (
@@ -67,8 +67,8 @@ CREATE CHANGE STREAM SingerAlbumStream FOR Singers, Albums;
   CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
 ) PRIMARY KEY(ID);
 
--- COMMENT Block 1
--- COMMENT Block 2
+-- COMMENT 1
+-- COMMENT 2
 
 CREATE CHANGE STREAM SingerAlbumStream FOR Singers, Albums;`,
 		},
