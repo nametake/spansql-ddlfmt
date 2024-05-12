6bbf8cf5373f5f733b9ef82ac12181b686949f97
nametake <nametake.kyarabuki@gmail.com>
2024-05-12 16:27:20 +0900

    Added stream ddl

--- format_test.go
+++ format_test.go
@@ -14,8 +14,8 @@ func TestFormatDDL(t *testing.T) {
 	}{
 		{
 			name: "Single DDL",
-			ddl:  `CREATE TABLE User (ID STRING(MAX) NOT NULL, Title STRING(MAX) NOT NULL, Artist STRING(MAX) NOT NULL, CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true)) PRIMARY KEY(ID);`,
-			want: `CREATE TABLE User (
+			ddl:  `CREATE TABLE Album (ID STRING(MAX) NOT NULL, Title STRING(MAX) NOT NULL, Artist STRING(MAX) NOT NULL, CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true)) PRIMARY KEY(ID);`,
+			want: `CREATE TABLE Album (
   ID STRING(MAX) NOT NULL,
   Title STRING(MAX) NOT NULL,
   Artist STRING(MAX) NOT NULL,
@@ -23,10 +23,10 @@ func TestFormatDDL(t *testing.T) {
 ) PRIMARY KEY(ID);`,
 		},
 		{
-			name: "Single DDL",
-			ddl: `CREATE TABLE User (ID STRING(MAX) NOT NULL, Title STRING(MAX) NOT NULL, Artist STRING(MAX) NOT NULL, CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true)) PRIMARY KEY(ID);
+			name: "Two DDL",
+			ddl: `CREATE TABLE Album (ID STRING(MAX) NOT NULL, Title STRING(MAX) NOT NULL, Artist STRING(MAX) NOT NULL, CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true)) PRIMARY KEY(ID);
 CREATE TABLE Artist (ID STRING(MAX) NOT NULL, Name STRING(MAX) NOT NULL, CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true)) PRIMARY KEY(ID);`,
-			want: `CREATE TABLE User (
+			want: `CREATE TABLE Album (
   ID STRING(MAX) NOT NULL,
   Title STRING(MAX) NOT NULL,
   Artist STRING(MAX) NOT NULL,
@@ -38,6 +38,20 @@ CREATE TABLE Artist (
   Name STRING(MAX) NOT NULL,
   CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
 ) PRIMARY KEY(ID);`,
+		},
+		{
+			name: "Stream DDL",
+			ddl: `CREATE TABLE Album (ID STRING(MAX) NOT NULL, Title STRING(MAX) NOT NULL, Artist STRING(MAX) NOT NULL, CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true)) PRIMARY KEY(ID);
+CREATE CHANGE STREAM SingerAlbumStream
+FOR Singers, Albums;`,
+			want: `CREATE TABLE Album (
+  ID STRING(MAX) NOT NULL,
+  Title STRING(MAX) NOT NULL,
+  Artist STRING(MAX) NOT NULL,
+  CreatedAt TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
+) PRIMARY KEY(ID);
+
+CREATE CHANGE STREAM SingerAlbumStream FOR Singers, Albums;`,
 		},
 	}
 
